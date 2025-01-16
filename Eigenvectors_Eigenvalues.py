17f13146952a963535e3e28b2f99d2eeb2039e93
BasaJess <email@jesusbasail.com>
2025-01-16 21:47:25 +0100

    changes

--- Eigenvectors_Eigenvalues.py
+++ Eigenvectors_Eigenvalues.py
@@ -1,9 +1,19 @@
 import numpy as np
 v = np.array([3, 1])
-E = np.array([[1, 0], [0, -1]])
+v2 = np.array([2,1])
+v3 = np.array([-3,-1])
+v4 = np.array([-1,1])
+A = np.array([[-1, 4], [2, -2]])
+E = np.array([[-1, 4], [2, -2]])
 Ev = np.dot(E, v)
 F = np.array([[-1, 0], [0, 1]])
 Fv = np.dot(F, v)
+V = np.concatenate((np.matrix(v).T, np.matrix(v2).T, np.matrix(v3).T,np.matrix(v4).T),axis=1)
+AV = np.dot(A, V)
+
+# function to convert column of matrix to 1D vector: 
+def vectorfy(mtrx, clmn):
+    return np.array(mtrx[:,clmn]).reshape(-1)
 
 import matplotlib.pyplot as plt
 def plot_vectors(vectors, colors):
@@ -36,9 +46,10 @@ def plot_vectors(vectors, colors):
                    angles='xy', scale_units='xy', scale=1, color=colors[i],)
 
 
-plot_vectors([v, Ev, Fv], ['lightblue', 'blue', 'darkblue'])
+plot_vectors([vectorfy(V, 0), Ev, Fv, vectorfy(V, 1), vectorfy(V, 2), vectorfy(V, 3),
+             vectorfy(AV, 0), vectorfy(AV, 1), vectorfy(AV, 2), vectorfy(AV, 3)], ['lightblue', 'blue', 'darkblue', 'green', 'gray', 'red', 'lightgreen', 'lightgray', 'orange', 'blue'])
 plt.xlim(-4, 5)
-_ = plt.ylim(-3, 3)
+_ = plt.ylim(-4, 5)
 
 plt.show()
 
