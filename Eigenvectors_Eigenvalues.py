f86aa5cf9dc606c84cf8772673e83b9325af52c1
BasaJess <email@jesusbasail.com>
2025-01-16 17:22:54 +0100

    changes

--- Eigenvectors_Eigenvalues.py
+++ Eigenvectors_Eigenvalues.py
@@ -20,15 +20,20 @@ def plot_vectors(vectors, colors):
     plt.xlim(-1, 4)
     plt.ylim(-1, 4)
     """
+
     plt.figure()
     plt.axvline(x=0, color='lightgray')
-    plt.axhline(y=0, color='lightgray')
+    plt.axhline(y=0, color='lightgray')  
 
     for i in range(len(vectors)):
         x = np.concatenate([[0,0],vectors[i]])
         plt.quiver([x[0]], [x[1]], [x[2]], [x[3]],
                    angles='xy', scale_units='xy', scale=1, color=colors[i],)
 
+
 plot_vectors([v], ['lightblue'])
 plt.xlim(-1, 5)
 _ = plt.ylim(-1, 5)
+
+plt.show()
+
